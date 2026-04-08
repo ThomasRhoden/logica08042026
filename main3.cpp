bf981cd9206b8eb29b6d7606c07d1e799c9847d6
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-08 20:56:59 -0300

    commit 4

---
+++ main3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    std::cout<<std::sqrt(64)<<std::endl;
+    std::cout<<std::round(2.6)<<std::endl;
+    std::cout<<std::log(2)<<std::endl;
+    
+    
+    return 0;
+}
