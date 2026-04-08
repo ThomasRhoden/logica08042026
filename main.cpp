bbd5a5fcd1e118d10e3677e34a9843de7accb5f7
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-08 20:33:40 -0300

    commit 2

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <cmath>
-
+#include <algorithm>
 
 int main(){
 
     int a = 5;
     int b = 3;
+    int c = 8;
 
-    std::cout<<std::max(a, b)<<std::endl;
+    std::cout<<std::max({a, b, c})<<std::endl;
 
 
 
