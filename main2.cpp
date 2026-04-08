f8aabf30e88adc14926d72e101ad1894e99aa918
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-08 20:35:21 -0300

    commit 3

---
+++ main2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    int a = 5;
+    int b = 3;
+
+    std::cout<<std::min(a, b)<<std::endl;
+    std::cout<<std::max(a, b)<<std::endl;
+    
+
+    return 0;
+}
