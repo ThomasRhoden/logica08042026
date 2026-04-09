5f0d6da9c002d44b48f51bd00528cd14b71784a5
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-08 21:34:32 -0300

    commit 5

---
+++ main4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    int a = 5;
+    int b = 2;
+
+    if(a > b){
+        std::cout<<"A é maior que B."<<std::endl;
+    }else{
+        std::cout<<"B é maior ou igual que A."<<std::endl;
+    }
+
+
+    return 0;
+}
