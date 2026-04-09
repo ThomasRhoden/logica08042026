9db657aeff9bf1cc1b5cf267c20fe044fc01b08f
ThomasRhoden <rhodenthomas@gmail.com>
2026-04-08 21:48:31 -0300

    commit 7

---
+++ main5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+int main(){
+
+    std::string user;
+    std::string senha;
+
+    std::cout<<"Digite seu usuario de acesso: "<<std::endl;
+    std::cin>>user;
+
+    std::cout<<"Digite sua senha: "<<std::endl;
+    std::cin>>senha;
+    if(senha == "123456789"){
+        std::cout<<"O usuario "<<user<<" esta logado com sucesso!!"<<std::endl;
+    }else{
+        std::cout<<"Usuario ou senha invalidos"<<std::endl;
+    }
+
+    return 0;
+}
